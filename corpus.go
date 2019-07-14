599df0753d84b3175515c3b5da6174b048ce39db
Miraç Gülgönül <mlg556@gmail.com>
2019-07-14 23:39:04 +0300

    removed Immob bool, changed mutator method prefixes to P, implemented MakeCorpus, ApplyForce and Update, added tests.

--- corpus.go
+++ corpus.go
@@ -1,7 +1,7 @@
 /*
 	Package corpus implements a 2D physical object "Corpus"
 	and a 2D vector "Vector" with related mathematical operations.
- */
+*/
 package corpus
 
 import (
@@ -9,16 +9,41 @@ import (
 )
 
 // Corpus is a 2D physical disk with vectors for position, velocity
-// acceleration, scalars for mass and radius, and a boolean state for immobility
+// acceleration, scalars for mass and radius.
 type Corpus struct {
 	Pos, Vel, Acc Vector
-	Mass, Radius float64
-	Immob bool
+	Mass, Radius  float64
 }
 
 // Vector is a 2D vector with x and y components of type float64.
 type Vector struct {
 	X, Y float64
+}
+
+// MakeCorpus initialises and returns a Corpus with
+// given Pos, Vel and Rad all in float64 forms.
+// Mass is Rad squared, Acc is 0, 0.
+func MakeCorpus(posX, posY, velX, velY, rad float64) Corpus {
+	c := Corpus{}
+	c.Pos = Vector{X: posX, Y: posY}
+	c.Vel = Vector{X: velX, Y: velY}
+	c.Radius = rad
+	c.Mass = rad * rad
+	c.Acc = Vector{0, 0}
+
+	return c
+}
+
+// ApplyForce subjects the corpus to the given force by mutating its acceleration.
+func (c *Corpus) ApplyForce(f Vector) {
+	c.Acc.AddP(f.Div(c.Mass)) // a = F/m
+}
+
+// Update updates the given corpus by mutating its physical attributes as unit time passes.
+func (c *Corpus) Update() {
+	c.Vel.AddP(c.Acc) // a = dv/dt
+	c.Pos.AddP(c.Vel) // v = dx/dt
+	c.Acc.MultP(0)    // resets acceleration
 }
 
 // Add adds two vectors, returning a new vector.
@@ -27,14 +52,14 @@ func (a Vector) Add(b Vector) Vector {
 }
 
 // AddI adds two vectors in place.
-func (a *Vector) AddI(b Vector) {
+func (a *Vector) AddP(b Vector) {
 	a.X = a.X + b.X
 	a.Y = a.Y + b.Y
 }
 
 // AngleBetween returns the angle between the two vectors in radians.
 func (a Vector) AngleBetween(b Vector) float64 {
-	return math.Acos(a.Dot(b) / (a.Mag()*b.Mag()))
+	return math.Acos(a.Dot(b) / (a.Mag() * b.Mag()))
 }
 
 // Div divides the vector with a scalar(float64), returning a new vector.
@@ -42,15 +67,15 @@ func (a Vector) Div(b float64) Vector {
 	return Vector{a.X / b, a.Y / b}
 }
 
-// DivI divides the vector with a scalar(float64) in place.
-func (a *Vector) DivI(b float64) {
+// DivP divides the vector with a scalar(float64) in place.
+func (a *Vector) DivP(b float64) {
 	a.X = a.X / b
 	a.Y = a.Y / b
 }
 
 // Dot returns the dot product of two vectors.
 func (a Vector) Dot(b Vector) float64 {
-	return a.X * b.X + a.Y * b.Y
+	return a.X*b.X + a.Y*b.Y
 }
 
 // Mag returns the magnitude of the vector
@@ -63,8 +88,8 @@ func (a Vector) Mult(b float64) Vector {
 	return Vector{a.X * b, a.Y * b}
 }
 
-// MultI multiplies the vector with a scalar(float64) in place.
-func (a *Vector) MultI(b float64) {
+// MultP multiplies the vector with a scalar(float64) in place.
+func (a *Vector) MultP(b float64) {
 	a.X = a.X * b
 	a.Y = a.Y * b
 }
@@ -74,9 +99,9 @@ func (a Vector) Norm() Vector {
 	return a.Div(a.Mag())
 }
 
-// NormI normalizes a vector in place.
-func (a *Vector) NormI() {
-	a.DivI(a.Mag())
+// NormP normalizes a vector in place.
+func (a *Vector) NormP() {
+	a.DivP(a.Mag())
 }
 
 // Sub substracts a from b, returning a new vector.
@@ -84,21 +109,8 @@ func (a Vector) Sub(b Vector) Vector {
 	return Vector{a.X - b.X, a.Y - b.Y}
 }
 
-// SubI substracts a from b in place.
-func (a *Vector) SubI(b Vector) {
+// SubP substracts a from b in place.
+func (a *Vector) SubP(b Vector) {
 	a.X = a.X - b.X
 	a.Y = a.Y - b.Y
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
--- corpus_test.go
+++ corpus_test.go
@@ -1,8 +1,86 @@
 package corpus
 
 import (
+	"math"
 	"testing"
 )
+
+func TestMakeCorpus(t *testing.T) {
+	posX := 1.0
+	posY := 2.0
+	velX := 3.0
+	velY := 4.0
+	rad := 5.0
+	c := MakeCorpus(posX, posY, velX, velY, rad)
+
+	if c.Pos.X != posX {
+		t.Error("WRONG POSX")
+	}
+
+	if c.Pos.Y != posY {
+		t.Error("WRONG POSY")
+	}
+
+	if c.Vel.X != velX {
+		t.Error("WRONG VELX")
+	}
+
+	if c.Vel.Y != velY {
+		t.Error("WRONG VELY")
+	}
+
+	if c.Radius != rad {
+		t.Error("WRONG RAD")
+	}
+
+}
+
+func TestCorpus_ApplyForce(t *testing.T) {
+	pos := Vector{0, 0}
+	vel := Vector{0, 0}
+	acc := Vector{0, 0}
+
+	mass := 1.0
+	radius := 1.0
+
+	c := Corpus{pos, vel, acc, mass, radius}
+
+	c.ApplyForce(Vector{1, 1})
+
+	res := Vector{1, 1}
+
+	if c.Acc != res {
+		t.Error("WRONG !!")
+	}
+}
+
+func TestCorpus_Update(t *testing.T) {
+	pos := Vector{0, 0}
+	vel := Vector{1, 1}
+	acc := Vector{1, 1}
+
+	mass := 1.0
+	radius := 1.0
+
+	c := Corpus{pos, vel, acc, mass, radius}
+	c.Update()
+
+	resPos := Vector{2, 2}
+	resVel := Vector{2, 2}
+	resAcc := Vector{0, 0}
+
+	if c.Pos != resPos {
+		t.Error("WRONG POS!!")
+	}
+
+	if c.Vel != resVel {
+		t.Error("WRONG VEL")
+	}
+
+	if c.Acc != resAcc {
+		t.Error("WRONG ACC")
+	}
+}
 
 func TestVector_Add(t *testing.T) {
 	a := Vector{1, 1}
@@ -16,10 +94,10 @@ func TestVector_Add(t *testing.T) {
 
 }
 
-func TestVector_AddI(t *testing.T) {
+func TestVector_AddP(t *testing.T) {
 	a := Vector{1, 1}
 	b := Vector{2, 2}
-	a.AddI(b)
+	a.AddP(b)
 	res := Vector{3, 3}
 
 	if a != res {
@@ -28,11 +106,119 @@ func TestVector_AddI(t *testing.T) {
 
 }
 
+func TestVector_AngleBetween(t *testing.T) {
+	a := Vector{1, 0}
+	b := Vector{0, 1}
+
+	res := 2 * math.Pi
+
+	if a.AngleBetween(b)-res > 0.001 {
+		t.Error("WRONG!!")
+	}
+}
+
+func TestVector_Div(t *testing.T) {
+	a := Vector{2, 2}
+	res := Vector{1, 1}
+
+	if a.Div(2) != res {
+		t.Error("WRONG !!")
+	}
+}
+
+func TestVector_DivP(t *testing.T) {
+	a := Vector{2, 2}
+	res := Vector{1, 1}
+
+	a.DivP(2)
+
+	if a != res {
+		t.Error("WRONG !!")
+	}
+}
+
+func TestVector_Dot(t *testing.T) {
+	a := Vector{1, 2}
+	b := Vector{3, 4}
+	res := 11.0
+
+	if a.Dot(b) != res {
+		t.Error("WRONG")
+	}
+}
+
 func TestVector_Mag(t *testing.T) {
 	a := Vector{3, 4}
 	res := 5.0
 
 	if a.Mag() != res {
 		t.Error("WRONG!!")
+	}
+}
+
+func TestVector_Mult(t *testing.T) {
+	a := Vector{3, 4}
+	b := 3.0
+
+	res := Vector{9, 12}
+
+	if a.Mult(b) != res {
+		t.Error("WRONG !!")
+	}
+}
+
+func TestVector_MultP(t *testing.T) {
+	a := Vector{3, 4}
+	b := 3.0
+	a.MultP(b)
+
+	res := Vector{9, 12}
+
+	if a != res {
+		t.Error("WRONG !!")
+	}
+}
+
+func TestVector_Norm(t *testing.T) {
+	a := Vector{10, 0}
+	res := Vector{1, 0}
+
+	if a.Norm() != res {
+		t.Error("WRONG !!")
+	}
+}
+
+func TestVector_NormP(t *testing.T) {
+	a := Vector{10, 0}
+	res := Vector{1, 0}
+
+	a.NormP()
+
+	if a != res {
+		t.Error("WRONG !!")
+	}
+}
+
+func TestVector_Sub(t *testing.T) {
+	a := Vector{3, 4}
+	b := Vector{1, 2}
+
+	res := Vector{2, 2}
+
+	if a.Sub(b) != res {
+		t.Error("WRONG !!")
+	}
+}
+
+func TestVector_SubP(t *testing.T) {
+	a := Vector{3, 4}
+	b := Vector{1, 2}
+
+	res := Vector{2, 2}
+
+	a.SubP(b)
+
+	if a != res {
+		t.Error("WRONG !!")
 	}
 }
